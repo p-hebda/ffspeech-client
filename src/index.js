0513394f6d4d94ba42025e0a4be5b0432af7528a
peterhebda <hebda24@gmail.com>
2023-03-19 11:50:45 -0500

    replace render w/ create root

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {render} from 'react-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import * as ReactDOMClient from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 
-render(
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
     <React.StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
