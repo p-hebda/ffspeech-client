f28beda593e12281d30545bfb5ad6362b1166cb0
peterhebda <hebda24@gmail.com>
2023-03-19 20:47:58 -0500

    refactor nav bar to support mobile

--- src/main/components/ButtonAppBar.js
+++ src/main/components/ButtonAppBar.js
@@ -4,8 +4,19 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import {useNavigate} from "react-router-dom";
+import {IconButton, Menu, MenuItem} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function ButtonAppBar() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     let navigate = useNavigate();
     const routeChange = (path) => {
         navigate(path);
@@ -16,21 +27,35 @@ export default function ButtonAppBar() {
         {name: "About", path: "/about"}
     ];
 
+    const currentPage = pages.find((page) => page.path === window.location.pathname);
     return (
         <Box sx={{flexGrow: 1}}>
-            <AppBar position="sticky" color='inherit'>
+            <AppBar position="static" color='inherit'>
                 <Toolbar>
-                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
-                        {pages.map(({name, path}) => (
-                            <Button
-                                key={name}
-                                onClick={() => routeChange(path)}
-                                sx={{my: 2, color: 'black', display: 'block'}}
-                            >
-                                {name}
-                            </Button>
-                        ))}
-                    </Box>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{mr: 2}}
+                        onClick={handleClick}>
+                        <MenuIcon/>
+                    </IconButton>
+                    {currentPage !== undefined ? currentPage.name : "Not Found"}
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        {pages.map(({name, path}) => <MenuItem key={name} onClick={() => {
+                            handleClose();
+                            routeChange(path);
+                        }}>{name}</MenuItem>)}
+                    </Menu>
                 </Toolbar>
             </AppBar>
         </Box>
